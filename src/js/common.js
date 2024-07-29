d2ba17f34fc111558707606b4c7fd00ad77a0f1a
梁怀刚 <lianghuaigang@xiaoduotech.com>
2024-07-29 20:47:17 +0800

    feat: 添加缓存过期时间

--- src/js/common.js
+++ src/js/common.js
@@ -34,29 +34,42 @@ function getQueryParam(key) {
 class LRUCache {
   #length;
   #cache;
-  constructor(length) {
+  constructor(length = 10) {
     this.#length = length;
     this.#cache = new Map();
+    this.clearCacheLoop();
+  }
+
+  clearCacheLoop() {
+    this.#cache.forEach((value, key) => {
+      const [, timeSamp] = value;
+      if (Date.now() > timeSamp) {
+        this.#cache.delete(key);
+      }
+    });
+    requestAnimationFrame(() => {
+      this.clearCacheLoop();
+    });
   }
 
   get(key) {
     if (!this.#cache.has(key)) {
       return;
     }
-    const value = this.#cache.get(key);
+    const [value, timeSamp] = this.#cache.get(key);
     this.#cache.delete(key);
-    this.#cache.set(key, value);
+    this.#cache.set(key, [value, timeSamp]);
     return value;
   }
 
-  set(key, value) {
+  set(key, value, expireTime = 10) {
     if (this.#cache.has(key)) {
       this.#cache.delete(key);
     }
     if (this.#cache.size === this.#length) {
       this.#cache.delete(this.#cache.keys().next().value);
     }
-    this.#cache.set(key, value);
+    this.#cache.set(key, [value, Date.now() + expireTime * 1000]);
   }
 }
 // https://api.github.com/search/repositories?q=stars%3A%3E1&sort=stars&order=desc&type=Repositories&page=1&per_page=10
